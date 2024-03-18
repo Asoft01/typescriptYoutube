9a0dcadd9ccaf925a981549de27303ff8e478cd1
Adeleke Hammed <lekhad19@gmail.com>
2024-03-18 14:17:14 +0100

    added my function

---
+++ myFunction.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    // return num + 2;
+    return "hello"; 
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid) {
+}
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+
+let myValue = addTwo(2);
+
+getUpper("adeleke");
+signUpUser("hammed", "adeleke", false);
+loginUser("h", "h@h.com");
--- myFunction.ts
+++ myFunction.ts
@@ -2,6 +2,20 @@ function addTwo(num: number) {
     return num + 2;
 }
 
+function getUpper(val: string) {
+    return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){
+
+}
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {
+
+}
+
 addTwo(2);
+getUpper("adeleke");
+signUpUser("hammed", "adeleke", false);
+loginUser("h", "h@h.com");
 
 export {};
